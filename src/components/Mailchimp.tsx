36cbd4acaa54587a912082236df1d5a4ee16c0d0
Ansh <anshdubeyjii@gmail.com>
2025-04-09 11:49:50 +0530

    intial commit

---
+++ src/components/Mailchimp.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+// This component has been cleaned to remove the Mailchimp newsletter section
+
+import { useState } from "react";
+
+// You can remove the `mailchimp` import if it's not used elsewhere
+// import { mailchimp } from "@/app/resources";
+
+// Also clean unused components from Once UI
+// import { Button, Flex, Heading, Input, Text, Background, Column } from "@/once-ui/components";
+
+// If this file is no longer needed, you can even delete it completely from your project
+// or repurpose it for another component.
+
+export const Mailchimp = () => {
+  return null; // or simply delete the component if not used
+};
---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import {
+  AvatarGroup,
+  Carousel,
+  Column,
+  Flex,
+  Heading,
+  SmartLink,
+  Text,
+} from "@/once-ui/components";
+
+interface ProjectCardProps {
+  href: string;
+  priority?: boolean;
+  images: string[];
+  title: string;
+  content: string;
+  description: string;
+  avatars: { src: string }[];
+  link: string;
+}
+
+export const ProjectCard: React.FC<ProjectCardProps> = ({
+  href,
+  images = [],
+  title,
+  content,
+  description,
+  avatars,
+  link,
+}) => {
+  return (
+    <Column fillWidth gap="m">
+    {/* No content to display */}
+  </Column>
+  );
+};
